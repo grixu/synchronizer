063ba5efdd74069a74d7b1f2a9afa05332443a57
Mateusz Gostaski <mateusz.gostanski@rywal.com.pl>
2020-12-07 15:42:51 +0100

    Updated config file: tow new variables for md5 checking feature

--- config/config.php
+++ config/config.php
@@ -5,5 +5,7 @@ return [
     'db_logging' => env('SYNCHRONIZER_DB_LOGGING',true),
     'log_turned_off_fields' => [
         'updated_at'
-    ]
+    ],
+    'md5_control' => env('SYNCHRONIZER_MD5_CONTROL', true),
+    'md5_local_model_field' => env('SYNCHRONIZER_MD5_FIELD', 'checksum'),
 ];
