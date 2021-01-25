211591b19e44b68774f69c48285b548b227e4a97
Mateusz Gostaski <mateusz.gostanski@rywal.com.pl>
2021-01-25 15:47:10 +0100

    Created Checksum class
    It's gathered functionalities previously putted in Synchronizer and Map classes. Now it's simpler and in one place. This class calculate md5 checksum, allowing to validate it's identical with that stored in model and update that.

---
+++ src/Checksum.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Grixu\Synchronizer;
+
+use Grixu\Synchronizer\Exceptions\EmptyMd5FieldNameInConfigException;
+use Illuminate\Database\Eloquent\Model;
+
+class Checksum
+{
+    protected ?string $md5 = null;
+
+    protected Map $map;
+    protected Model $model;
+
+    public function __construct(Map $map, Model $model)
+    {
+        $this->map = $map;
+        $this->model = $model;
+    }
+
+    protected function calculate(): void
+    {
+        $this->md5 = md5(
+            json_encode(
+                $this->model->only(
+                    $this->map->getModelFieldsArray()
+                )
+            )
+        );
+    }
+
+    public function validate()
+    {
+        if (config('synchronizer.checksum_control') == false || empty(config('synchronizer.checksum_control'))) {
+            return false;
+        }
+
+        $md5FieldName = $this->getMd5FieldName();
+
+        if (empty($this->model->$md5FieldName)) {
+            return false;
+        }
+
+        return $this->model->$md5FieldName === $this->getMd5();
+    }
+
+    protected function getMd5FieldName(): string
+    {
+        $md5FieldName = config('synchronizer.checksum_field');
+        if (empty($md5FieldName)) {
+            throw new EmptyMd5FieldNameInConfigException();
+        }
+
+        return $md5FieldName;
+    }
+
+    public function update(): void
+    {
+        $md5FieldName = $this->getMd5FieldName();
+        $this->model->$md5FieldName = $this->getMd5();
+        $this->model->save();
+    }
+
+    public function getMd5(): string
+    {
+        if (empty($this->md5)) {
+            $this->calculate();
+        }
+
+        return $this->md5;
+    }
+}
