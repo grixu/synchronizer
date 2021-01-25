f9c62757e888aa50dd66e30d5dff391dea114446
Mateusz Gostaski <mateusz.gostanski@rywal.com.pl>
2021-01-25 15:55:53 +0100

    Updated SynchronizerServiceProvider

--- src/SynchronizerServiceProvider.php
+++ src/SynchronizerServiceProvider.php
@@ -4,24 +4,12 @@ namespace Grixu\Synchronizer;
 
 use Grixu\Synchronizer\Console\AddExcludedFieldCommand;
 use Grixu\Synchronizer\Console\ListExcludedFieldsCommand;
-use Grixu\Synchronizer\Console\SendSumUpCommand;
-use Grixu\Synchronizer\Console\SetBeginningForSumUpsCommand;
 use Illuminate\Support\ServiceProvider;
 
-/**
- * Class SynchronizerServiceProvider
- * @package Grixu\Synchronizer
- */
 class SynchronizerServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         if ($this->app->runningInConsole()) {
@@ -32,27 +20,19 @@ class SynchronizerServiceProvider extends ServiceProvider
                 'config'
             );
 
-            // Registering package commands.
             $this->commands(
                 [
                     AddExcludedFieldCommand::class,
                     ListExcludedFieldsCommand::class,
-                    SendSumUpCommand::class,
-                    SetBeginningForSumUpsCommand::class
                 ]
             );
         }
     }
 
-    /**
-     * Register the application services.
-     */
     public function register()
     {
-        // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'synchronizer');
 
-        // Register the main class to use with the facade
         $this->app->singleton(
             'synchronizer',
             function () {
