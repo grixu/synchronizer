424fdbd9b8adcfaac1ff5ac7ac30a67e6dfffa19
Mateusz Gostaski <mateusz.gostanski@rywal.com.pl>
2021-01-25 15:38:13 +0100

    Refactored models & migrations
    Refactored to simpler names without Synchronizer in model name. But still using synchronizer_ in table names due to avoid problems with projects tables.

--- src/Models/Log.php
+++ src/Models/Log.php
@@ -2,22 +2,15 @@
 
 namespace Grixu\Synchronizer\Models;
 
-use Grixu\Synchronizer\DataTransferObjects\SynchronizerLogData;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
 
 /**
- * Class SynchronizerLog
  * @property string model
  * @property int model_id
- * @property SynchronizerLogData log
- * @package Grixu\Synchronizer\Models
  * @method static create(array $array)
  */
-class SynchronizerLog extends Model
+class Log extends Model
 {
-    use Notifiable;
-
     public $timestamps = true;
 
     public $table = 'synchronizer_logs';
