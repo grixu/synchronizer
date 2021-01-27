77409a2a55664c8c7fc47b6b91d96068f5ab075b
Mateusz Gostaski <mateusz.gostanski@rywal.com.pl>
2021-01-27 12:24:23 +0100

    Created RelationshipSynchronizer

---
+++ src/RelationshipSynchronizer.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Grixu\Synchronizer;
+
+use Grixu\RelationshipDataTransferObject\RelationshipData;
+use Grixu\RelationshipDataTransferObject\RelationshipDataCollection;
+use Grixu\Synchronizer\Exceptions\WrongLocalModelException;
+use Grixu\Synchronizer\Exceptions\WrongRelationTypeException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class RelationshipSynchronizer
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function sync(RelationshipDataCollection $relationships): void
+    {
+        foreach ($relationships as $relationship) {
+            $this->syncRelationship($relationship);
+        }
+    }
+
+    public function syncRelationship(RelationshipData $relationshipData): void
+    {
+        $this->checkModelClass($relationshipData->localClass);
+
+        $localMethod = $relationshipData->localRelationshipName;
+        $this->checkRelationType($localMethod, $relationshipData->type);
+
+        $foreignKeys = $this->loadForeignKeys($relationshipData);
+
+        switch ($relationshipData->type) {
+            case BelongsTo::class:
+                $this->associateBelongsTo($this->model->$localMethod(), $foreignKeys[0]);
+                break;
+            case BelongsToMany::class:
+                $this->attachManyToMany($this->model->$localMethod(), $foreignKeys);
+        }
+
+        $this->model->save();
+    }
+
+    private function checkModelClass(string $localModelClass): void
+    {
+        if (get_class($this->model) !== $localModelClass)
+            throw new WrongLocalModelException();
+    }
+
+    private function checkRelationType(string $localKey, string $relationType): void
+    {
+        if (get_class($this->model->$localKey()) !== $relationType)
+            throw new WrongRelationTypeException();
+    }
+
+    private function loadForeignKeys(RelationshipData $relationshipData): array
+    {
+        $foreignKeys = $relationshipData->foreignKeys;
+        if (empty($foreignKeys)) {
+            $foreignKeys = [$relationshipData->foreignKey];
+        }
+
+        $foreignModel = $relationshipData->foreignClass;
+        $databaseForeignKeys = [];
+
+        foreach ($foreignKeys as $foreignKey) {
+            /** @var Model $localCopyOfForeignModel */
+            $localCopyOfForeignModel = $foreignModel::query()
+                ->where($relationshipData->foreignRelatedFieldName, $foreignKey)
+                ->first();
+
+            if (!empty($localCopyOfForeignModel)) {
+                $databaseForeignKeys[] = $localCopyOfForeignModel->getKey();
+            }
+        }
+
+        return $databaseForeignKeys;
+    }
+
+    private function associateBelongsTo(BelongsTo $relationship, int $foreignKey): void
+    {
+        $relationship->associate($foreignKey);
+    }
+
+    private function attachManyToMany(BelongsToMany $relationship, array $foreignKeys): void
+    {
+        $relationship->sync($foreignKeys);
+    }
+}
