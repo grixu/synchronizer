89ea1de07a4fea749c7a3345f489d495aaffef94
Mateusz Gostański <mg@grixu.dev>
2021-02-23 11:01:18 +0100

    Created ModelCreatedEvent & ModelSynchronizedEvent with test cases

---
+++ src/Events/ModelCreatedEvent.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Grixu\Synchronizer\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ModelCreatedEvent
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
+
+    public string $model;
+
+    public function __construct(string $model)
+    {
+        $this->model = $model;
+    }
+}
