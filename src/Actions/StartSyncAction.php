98678bbaa3a5c6958e1b99391cb3e1ef02fc1cc9
Mateusz Gostanski <mg@grixu.dev>
2021-03-22 15:31:19 +0100

    Updated StartSyncAction
    Dived code to methods to better apply SRP + bug fixed with empty errorHandler

--- src/Actions/StartSyncAction.php
+++ src/Actions/StartSyncAction.php
@@ -6,20 +6,15 @@ use Grixu\Synchronizer\Config\SyncConfig;
 use Grixu\Synchronizer\Events\CollectionSynchronizedEvent;
 use Grixu\Synchronizer\Jobs\LoadDataToSyncJob;
 use Illuminate\Bus\Batch;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Bus;
 
 class StartSyncAction
 {
-    public function execute(array $config, string $queue = 'default'): Batch
+    public function execute(SyncConfig|array $config, string $queue = 'default'): Batch
     {
-        $configCollection = collect($config);
-        $configCollection = $configCollection->map(fn ($item) => SyncConfig::make(...$item));
-
-        $jobs = [];
-
-        foreach ($configCollection as $config) {
-            $jobs[] = new LoadDataToSyncJob($config);
-        }
+        $configCollection = $this->prepareConfig($config);
+        $jobs = $this->prepareJobs($configCollection);
 
         return Bus::batch($jobs)
             ->allowFailures()
@@ -30,9 +25,35 @@ class StartSyncAction
                 }
             })
             ->catch(function (Batch $batch, \Throwable $exception) use ($configCollection) {
-                $configCollection->each(fn ($config) => $config->getErrorHandler());
+                $configCollection->each(function ($config) use ($exception) {
+                    if ($config->getErrorHandler() != null)
+                        $config->getErrorHandler()($exception);
+                });
             })
             ->onQueue($queue)
             ->dispatch();
+    }
+
+    protected function prepareConfig(SyncConfig|array $config): Collection
+    {
+        if (is_array($config)) {
+            $configCollection = collect($config);
+            $configCollection = $configCollection->map(fn ($item) => SyncConfig::make(...$item));
+        } else {
+            $configCollection = collect([$config]);
+        }
+
+        return $configCollection;
+    }
+
+    protected function prepareJobs(Collection $configCollection): array
+    {
+        $jobs = [];
+
+        foreach ($configCollection as $config) {
+            $jobs[] = new LoadDataToSyncJob($config);
+        }
+
+        return $jobs;
     }
 }
