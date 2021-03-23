523216b4626f627f90319aebc1cc9a73a7fb8d7c
Mateusz Gostanski <mg@grixu.dev>
2021-03-23 15:22:46 +0100

    Updated test case for SyncConfig

--- tests/Config/SyncConfigTest.php
+++ tests/Config/SyncConfigTest.php
@@ -9,39 +9,10 @@ use Grixu\Synchronizer\Tests\Helpers\FakeLoader;
 use Grixu\Synchronizer\Tests\Helpers\FakeParser;
 use Grixu\Synchronizer\Tests\Helpers\TestCase;
 use Illuminate\Support\Collection;
-use JetBrains\PhpStorm\Pure;
 use Throwable;
 
 class SyncConfigTest extends TestCase
 {
-    /** @test */
-    public function it_could_be_made_by_make_method()
-    {
-        $obj = $this->makeObj();
-
-        $this->assertEquals(SyncConfig::class, $obj::class);
-    }
-
-    #[Pure]
-    protected function makeObj(): SyncConfig
-    {
-        return SyncConfig::make(
-            FakeLoader::class,
-            FakeParser::class,
-            Language::class,
-            'xlId'
-        );
-    }
-
-    /** @test */
-    public function it_could_set_ids_to_sync()
-    {
-        $obj = $this->makeObj();
-        $obj->setIdsToSync([1, 2, 3]);
-
-        $this->assertNotEmpty($obj->getIdsToSync());
-    }
-
     /** @test */
     public function it_could_take_every_argument()
     {
