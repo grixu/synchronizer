025c1c1201d92bd8385cffb42c49ad071452cc4a
Mateusz Gostanski <mg@grixu.dev>
2021-03-23 13:29:29 +0100

    Updated config file

--- config/config.php
+++ config/config.php
@@ -3,10 +3,14 @@
 return [
     'send_slack_sum_up' => env('SYNCHRONIZER_SLACK_SUM_UP', false),
     'db_logging' => env('SYNCHRONIZER_DB_LOGGING',true),
+
     'timestamps' => [
         'updatedAt'
     ],
+
     'checksum_control' => env('SYNCHRONIZER_MD5_CONTROL', true),
     'checksum_field' => env('SYNCHRONIZER_MD5_FIELD', 'checksum'),
+    'checksum_timestamps_excluded' => false,
+
     'default_chunk_size' => env('SYNCHRONIZER_CHUNK_SIZE', 250),
 ];
