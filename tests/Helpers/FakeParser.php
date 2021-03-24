76fdb946dec508303732b1b5f0cc86badb4cad26
Mateusz Gostanski <mg@grixu.dev>
2021-03-24 14:34:48 +0100

    Updated FakeParser

--- tests/Helpers/FakeParser.php
+++ tests/Helpers/FakeParser.php
@@ -3,29 +3,28 @@
 namespace Grixu\Synchronizer\Tests\Helpers;
 
 use Grixu\SociusModels\Customer\DataTransferObjects\CustomerData;
-use Grixu\Synchronizer\Contracts\ParserInterface;
+use Grixu\Synchronizer\Abstracts\AbstractParser;
 use Illuminate\Database\Eloquent\Model;
 
-class FakeParser implements ParserInterface
+class FakeParser extends AbstractParser
 {
-    public function parse(Model $fakeModel): CustomerData
+    public function parseElement(Model $model): CustomerData
     {
-        ray($fakeModel);
         return new CustomerData(
             [
-                'name' => $fakeModel->knt_nazwa1 . ' ' . $fakeModel->knt_nazwa2,
-                'country' => $fakeModel->knt_kraj,
-                'postalCode' => $fakeModel->knt_kodp,
-                'city' => $fakeModel->knt_miasto,
-                'street' => $fakeModel->knt_ulica,
-                'vatNumber' => $fakeModel->knt_nip,
-                'district' => $fakeModel->knt_powiat,
-                'voivodeship' => $fakeModel->knt_wojewodztwo,
-                'phone1' => $fakeModel->knt_telefon1,
-                'phone2' => $fakeModel->knt_telefon2,
-                'email' => $fakeModel->knt_email,
-                'paymentPeriod' => (int) $fakeModel->knt_limitokres,
-                'xlId' => (int) $fakeModel->knt_gidnumer,
+                'name' => $model->knt_nazwa1 . ' ' . $model->knt_nazwa2,
+                'country' => $model->knt_kraj,
+                'postalCode' => $model->knt_kodp,
+                'city' => $model->knt_miasto,
+                'street' => $model->knt_ulica,
+                'vatNumber' => $model->knt_nip,
+                'district' => $model->knt_powiat,
+                'voivodeship' => $model->knt_wojewodztwo,
+                'phone1' => $model->knt_telefon1,
+                'phone2' => $model->knt_telefon2,
+                'email' => $model->knt_email,
+                'paymentPeriod' => (int) $model->knt_limitokres,
+                'xlId' => (int) $model->knt_gidnumer,
                 'syncTs' => now()->subYear(),
                 'updatedAt' => now(),
             ]
