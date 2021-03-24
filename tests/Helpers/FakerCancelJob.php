541c801538dd6f5068cc1abd4e0ca7ec214f30bc
Mateusz Gostanski <mg@grixu.dev>
2021-03-24 09:11:36 +0100

    Created FakerCancelJob

---
+++ tests/Helpers/FakerCancelJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Grixu\Synchronizer\Tests\Helpers;
+
+use Illuminate\Bus\Batchable;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class FakerCancelJob implements ShouldQueue
+{
+    use Batchable;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    public function handle()
+    {
+        if ($this->batch()) {
+            $this->batch()->cancel();
+        }
+    }
+}
