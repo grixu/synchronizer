900f3785e39b9ee29055e6f232037f2c4fefd576
Mateusz Gostanski <mg@grixu.dev>
2021-04-27 12:46:51 +0200

    Updated FakeExtendedModel

--- tests/Helpers/FakeExtendedModel.php
+++ tests/Helpers/FakeExtendedModel.php
@@ -2,11 +2,25 @@
 
 namespace Grixu\Synchronizer\Tests\Helpers;
 
+use Grixu\SociusModels\Product\Models\Brand;
 use Grixu\SociusModels\Product\Models\Product;
 use Grixu\Synchronizer\Attributes\SynchronizeWith;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[SynchronizeWith(Product::class)]
-class FakeExtendedModel extends Product
+class FakeExtendedModel extends Model
 {
     protected $table = 'products';
+
+    protected $guarded = ['id'];
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(
+            Brand::class,
+            'brand_id',
+            'id'
+        );
+    }
 }
