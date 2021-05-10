2e9346ca482b76db7ba231879fc1b812850ff8e7
Mateusz Gostanski <mg@grixu.dev>
2021-05-10 14:14:50 +0200

    Created LoggerNotification

---
+++ src/Notifications/LoggerNotification.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Grixu\Synchronizer\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\SlackMessage;
+use Illuminate\Notifications\Notification;
+
+class LoggerNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public function __construct(public string $model, public int $totalChanges)
+    {
+    }
+
+    public function via(): array
+    {
+        return ['slack'];
+    }
+
+    public function toSlack($notifiable): SlackMessage
+    {
+        return (new SlackMessage)
+            ->content(
+                'Synchronizacja modelu ' . $this->model . ' zakończona. Wprowadzonych zmian: ' . $this->totalChanges
+            );
+    }
+}
