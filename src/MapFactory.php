01629ecd985c5c671cd94dbac63a3a8c265599bf
Mateusz Gostanski <mg@grixu.dev>
2021-05-10 14:13:37 +0200

    Updated MapFactory

--- src/MapFactory.php
+++ src/MapFactory.php
@@ -2,35 +2,22 @@
 
 namespace Grixu\Synchronizer;
 
-use Illuminate\Support\Str;
 use Spatie\DataTransferObject\DataTransferObject;
 
 class MapFactory
 {
     public static function makeFromDto(DataTransferObject $dataTransferObject, string $model): Map
     {
-        $map = [];
-
-        foreach ($dataTransferObject->except('relationships')->toArray() as $key => $value) {
-            $map[$key] = Str::snake($key);
-        }
-
-        return new Map($map, $model);
+        return new Map(array_keys($dataTransferObject->except('relationships')->toArray()), $model);
     }
 
     public static function makeFromArray(array $dataArray, string $model): Map
     {
-        $map = [];
-
-        foreach ($dataArray as $key => $value) {
-            $map[$key] = Str::snake($key);
-        }
-
-        return new Map($map, $model);
+        return new Map(array_keys($dataArray), $model);
     }
 
-    public static function make(array $map, string $model): Map
+    public static function make(array $fields, string $model): Map
     {
-        return new Map($map, $model);
+        return new Map($fields, $model);
     }
 }
