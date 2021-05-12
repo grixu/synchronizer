03483623a93ae2583e01686ecfe65bf4620fe217
Mateusz Gostanski <mg@grixu.dev>
2021-05-12 09:22:49 +0200

    Added optimization for timestamps fields loading in AbstractParser

--- src/Abstracts/AbstractParser.php
+++ src/Abstracts/AbstractParser.php
@@ -5,19 +5,23 @@ namespace Grixu\Synchronizer\Abstracts;
 use Grixu\Synchronizer\Checksum;
 use Grixu\Synchronizer\Contracts\ParserInterface;
 use Grixu\Synchronizer\Contracts\SingleElementParserInterface;
+use Grixu\Synchronizer\Map;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 abstract class AbstractParser implements ParserInterface, SingleElementParserInterface
 {
     public function parse(Collection $collection): Collection
     {
         $timestampExcluded = config('synchronizer.checksum.timestamps_excluded');
+        $timestamps = Map::getTimestamps();
+        $timestamps = array_map(fn ($item) => Str::camel($item), $timestamps);
 
-        return $collection->map(function ($item) use ($timestampExcluded) {
+        return $collection->map(function ($item) use ($timestampExcluded, $timestamps) {
             $item = $this->parseElement($item);
 
             if (!$timestampExcluded) {
-                $item = $item->except('synchronizer.sync.timestamps');
+                $item = $item->except(...$timestamps);
             }
 
             $item->checksum = Checksum::generate($item->toArray());
