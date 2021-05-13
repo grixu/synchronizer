4c1f28b5f14b82c91dab9a2222b1939141912128
Mateusz Gostanski <mg@grixu.dev>
2021-05-13 11:44:53 +0200

    Updated Engine interface

--- src/Contracts/Engine.php
+++ src/Contracts/Engine.php
@@ -2,10 +2,11 @@
 
 namespace Grixu\Synchronizer\Contracts;
 
+use Grixu\Synchronizer\Transformer;
 use Illuminate\Support\Collection;
 
 interface Engine
 {
-    public function sync();
+    public function sync(Transformer|null $transformer = null);
     public function getIds(): Collection;
 }
