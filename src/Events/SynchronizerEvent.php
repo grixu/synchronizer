91ef68e12bf9a9277fc634aebf784b7d67b994df
Mateusz Gostanski <mg@grixu.dev>
2021-05-13 15:29:43 +0200

    Created SynchronizerEvent

---
+++ src/Events/SynchronizerEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Grixu\Synchronizer\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+
+class SynchronizerEvent
+{
+    use Dispatchable;
+    use InteractsWithSockets;
+
+    public function __construct(public array $changed)
+    {}
+}
