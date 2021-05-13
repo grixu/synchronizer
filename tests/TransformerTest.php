f5862d53aeff6102a64bbf5c0a2fcae3255ba77e
Mateusz Gostanski <mg@grixu.dev>
2021-05-13 10:02:59 +0200

    Refactored test case for Transformer

--- tests/TransformerTest.php
+++ tests/TransformerTest.php
@@ -4,19 +4,20 @@ namespace Grixu\Synchronizer\Tests;
 
 use Grixu\SociusModels\Product\Factories\ProductDataFactory;
 use Grixu\SociusModels\Product\Models\Product;
+use Grixu\Synchronizer\Checksum;
+use Grixu\Synchronizer\Transformer;
 use Grixu\Synchronizer\Map;
 use Grixu\Synchronizer\MapFactory;
-use Grixu\Synchronizer\ModelSynchronizer;
 use Grixu\Synchronizer\Tests\Helpers\MigrateProductsTrait;
 use Grixu\Synchronizer\Tests\Helpers\TestCase;
 
-class ModelSynchronizerTest extends TestCase
+class TransformerTest extends TestCase
 {
     use MigrateProductsTrait;
 
     protected array $input;
     protected Map $map;
-    protected ModelSynchronizer $obj;
+    protected Transformer $obj;
 
     protected function setUp(): void
     {
@@ -30,7 +31,9 @@ class ModelSynchronizerTest extends TestCase
     /** @test */
     public function it_create_array_to_sync()
     {
-        $obj = new ModelSynchronizer($this->map, null);
+        Checksum::setChecksumField(null);
+        $this->map = MapFactory::makeFromArray($this->input, Product::class);
+        $obj = new Transformer($this->map);
         $returnedData = $obj->sync($this->input);
 
         $this->assertNotEmpty($returnedData);
@@ -40,11 +43,12 @@ class ModelSynchronizerTest extends TestCase
     /** @test */
     public function it_create_array_to_sync_with_checksum()
     {
+        Checksum::setChecksumField(config('synchronizer.checksum.field'));
         $this->input['checksum'] = 'aaaa';
-        $obj = new ModelSynchronizer($this->map, config('synchronizer.checksum.field'));
+        $obj = new Transformer($this->map);
         $returnedData = $obj->sync($this->input);
 
         $this->assertNotEmpty($returnedData);
-        $this->assertCount(count($this->map->get()) + 1, $returnedData);
+        $this->assertCount(count($this->map->get()), $returnedData);
     }
 }
