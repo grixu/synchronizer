01f0e4c6ef206d28cb4cb69fbbf3b68cc8dd15ea
Mateusz Gostanski <mg@grixu.dev>
2021-05-14 10:21:48 +0200

    Added $modelKey in BaseEngine

--- src/Abstracts/BaseEngine.php
+++ src/Abstracts/BaseEngine.php
@@ -5,17 +5,19 @@ namespace Grixu\Synchronizer\Abstracts;
 use Grixu\Synchronizer\Contracts\Engine;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 abstract class BaseEngine implements Engine
 {
     protected Collection $ids;
     protected Model $model;
-
+    protected string $modelKey;
 
     public function __construct(protected Collection $input, protected string $key, string $model)
     {
         $this->ids = collect();
         $this->model = new $model;
+        $this->modelKey = Str::snake($key);
 
         $this->filterByKeyExistence();
     }
