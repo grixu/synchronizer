2586b496f85444372baf86f49f854c86f4ab4117
Mateusz Gostanski <mg@grixu.dev>
2021-05-17 14:50:28 +0200

    Deleted unused Exceptions

--- src/Exceptions/WrongLocalModelException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Grixu\Synchronizer\Exceptions;
-
-use Exception;
-
-class WrongLocalModelException extends Exception
-{
-}
--- src/Exceptions/WrongRelationTypeException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Grixu\Synchronizer\Exceptions;
-
-use Exception;
-
-class WrongRelationTypeException extends Exception
-{
-}
