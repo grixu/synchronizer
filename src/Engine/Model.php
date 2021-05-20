7f7c2eb4b56f33cdd6a53d0899385918de9b5393
Mateusz Gostanski <mg@grixu.dev>
2021-05-20 13:35:17 +0200

    Updated Model Engine
    Added cleaning checksums when input data have any relations.

--- src/Engine/Model.php
+++ src/Engine/Model.php
@@ -2,6 +2,7 @@
 
 namespace Grixu\Synchronizer\Engine;
 
+use Grixu\Synchronizer\Checksum;
 use Grixu\Synchronizer\Engine\Abstracts\BaseEngine;
 use Grixu\Synchronizer\Engine\Contracts\Transformer;
 
@@ -13,9 +14,19 @@ class Model extends BaseEngine
             return;
         }
 
-        $transformed = $this->input->map(function ($item) use ($transformer) {
-            return $transformer->sync($item);
-        });
+        $checkRelations = $this->getAllRelations($transformer);
+
+        $resetChecksum = count($checkRelations) > 0;
+
+        $transformed = $this->input->map(
+            function ($item) use ($resetChecksum, $transformer) {
+                if ($resetChecksum) {
+                    $item[Checksum::$checksumField] = null;
+                }
+
+                return $transformer->sync($item);
+            }
+        );
 
         $fields = array_diff($transformer->getMap()->getModelFieldsArray(), [$this->modelKey]);
 
