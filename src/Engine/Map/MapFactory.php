1f2b8d9c9cbbfab70df9ea933400e505fe88bf5b
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-08-20 08:57:19 +0200

    Updated MapFactory to accept new param

--- src/Engine/Map/MapFactory.php
+++ src/Engine/Map/MapFactory.php
@@ -6,18 +6,18 @@ use Spatie\DataTransferObject\DataTransferObject;
 
 class MapFactory
 {
-    public static function makeFromDto(DataTransferObject $dataTransferObject, string $model): Map
+    public static function makeFromDto(DataTransferObject $dataTransferObject, string $model, string|null $checksumField = null): Map
     {
-        return new Map(array_keys($dataTransferObject->toArray()), $model);
+        return new Map(array_keys($dataTransferObject->toArray()), $model, $checksumField);
     }
 
-    public static function makeFromArray(array $dataArray, string $model): Map
+    public static function makeFromArray(array $dataArray, string $model, string|null $checksumField = null): Map
     {
-        return new Map(array_keys($dataArray), $model);
+        return new Map(array_keys($dataArray), $model, $checksumField);
     }
 
-    public static function make(array $fields, string $model): Map
+    public static function make(array $fields, string $model, string|null $checksumField = null): Map
     {
-        return new Map($fields, $model);
+        return new Map($fields, $model, $checksumField);
     }
 }
