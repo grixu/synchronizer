c223ae596c8ab751d2b4d0d08ce488ecb83d3f32
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-08-26 12:55:31 +0200

    Created unit test for EngineConfig

---
+++ tests/Config/EngineConfigTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Grixu\Synchronizer\Tests\Config;
+
+use Grixu\SociusModels\Description\Models\Language;
+use Grixu\Synchronizer\Config\Contracts\EngineConfig as EngineConfigInterface;
+use Grixu\Synchronizer\Config\EngineConfig;
+use Grixu\Synchronizer\Tests\Helpers\TestCase;
+
+class EngineConfigTest extends TestCase
+{
+    /** @test */
+    public function it_could_take_every_argument()
+    {
+        $obj = $this->createObj();
+
+        $this->assertEquals(EngineConfig::class, $obj::class);
+    }
+
+    protected function createObj($timestamps = [], $excludedFields = [], $checksumField = 'checksum', $ids = []): EngineConfig
+    {
+        return new EngineConfig(
+            model: Language::class,
+            key: 'xlId',
+            excludedFields: $excludedFields,
+            checksumField: $checksumField,
+            timestamps: $timestamps,
+            ids: $ids
+        );
+    }
+
+    /** @test */
+    public function it_provide_access_to_checksum_field_name()
+    {
+        $obj = $this->createObj();
+        $returnedValue = $obj->getChecksumField();
+
+        $this->assertEquals('checksum', $returnedValue);
+    }
+
+    /**
+     * @test
+     * @environment-setup useDisabledChecksum
+     */
+    public function it_not_allows_checksum_field_if_checking_system_is_off()
+    {
+        $obj = $this->createObj();
+        $this->assertEmpty($obj->getChecksumField());
+    }
+
+    protected function useDisabledChecksum($app)
+    {
+        $app->config->set('synchronizer.checksum.control', false);
+    }
+
+    /** @test */
+    public function it_provide_access_to_timestamp_fields()
+    {
+        $timestamps = ['one'];
+        $obj = $this->createObj(timestamps: $timestamps);
+
+        $this->assertNotEmpty($obj->getTimestamps());
+        $this->assertEquals($timestamps, $obj->getTimestamps());
+    }
+
+    /**
+     * @test
+     * @environment-setup useDisabledChecksum
+     */
+    public function it_block_access_to_timestamps_when_checksum_checking_is_disabled()
+    {
+        $timestamps = ['one'];
+        $obj = $this->createObj(timestamps: $timestamps);
+
+        $this->assertEmpty($obj->getTimestamps());
+    }
+
+    /** @test */
+    public function it_provide_access_to_ids_array()
+    {
+        $ids = [1,2,3];
+        $obj = $this->createObj(ids: $ids);
+        $returnedValue = $obj->getIds();
+
+        $this->assertEquals($ids, $returnedValue);
+    }
+
+    /** @test */
+    public function it_is_singleton()
+    {
+        $obj = EngineConfig::getInstance();
+
+        $this->assertTrue($obj instanceof EngineConfigInterface);
+    }
+
+    /** @test */
+    public function it_is_singleton_and_could_set_instance()
+    {
+        $instanceBefore = EngineConfig::getInstance();
+        $obj = $this->createObj();
+        EngineConfig::setInstance($obj);
+
+        $this->assertNotEquals($instanceBefore, EngineConfig::getInstance());
+        $this->assertEquals($obj, app(EngineConfigInterface::class));
+    }
+}
