5060ff08046650a66b315aff0400017136ec8174
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-08-26 15:10:23 +0200

    Updated FakeProcessConfig

--- tests/Helpers/FakeProcessConfig.php
+++ tests/Helpers/FakeProcessConfig.php
@@ -3,30 +3,28 @@
 namespace Grixu\Synchronizer\Tests\Helpers;
 
 use Grixu\Synchronizer\Config\ProcessConfig;
+use Grixu\Synchronizer\Process\Handlers\DefaultErrorHandler;
+use Grixu\Synchronizer\Process\Handlers\DefaultSyncHandler;
 
 class FakeProcessConfig
 {
-    public static function make(array $config = []): ProcessConfig
+    public static function make(string $jobs = 'default', string|null $sync = null, string|null $error = null): ProcessConfig
     {
-        if (empty($config)) {
-            $config = config('synchronizer.jobs.default');
+        if (empty($sync)) {
+            $sync = DefaultSyncHandler::class;
+        }
+
+        if (empty($error)) {
+            $error = DefaultErrorHandler::class;
         }
 
         return ProcessConfig::make(
-            FakeLoader::class,
-            FakeParser::class,
-            $config,
+            loaderClass: FakeLoader::class,
+            parserClass: FakeParser::class,
+            jobsConfig: $jobs,
+            syncHandler: $sync,
+            errorHandler: $error
         );
-    }
-
-    public static function makeLoadAllAndParse(): ProcessConfig
-    {
-        return self::make(config('synchronizer.jobs.load-all-and-parse'));
-    }
-
-    public static function makeChunkLoadAndParse(): ProcessConfig
-    {
-        return self::make(config('synchronizer.jobs.chunk-load-and-parse'));
     }
 
     public static function makeArray(): array
