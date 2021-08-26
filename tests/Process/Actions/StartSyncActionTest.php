eff9cdbcee27e36b31295deda3027713439413da
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-08-26 15:10:46 +0200

    Updated StartSyncAction unit test

--- tests/Process/Actions/StartSyncActionTest.php
+++ tests/Process/Actions/StartSyncActionTest.php
@@ -4,13 +4,12 @@ namespace Grixu\Synchronizer\Tests\Process\Actions;
 
 use Grixu\Synchronizer\Process\Actions\StartSyncAction;
 use Grixu\Synchronizer\Process\Events\CollectionSynchronizedEvent;
-use Grixu\Synchronizer\Tests\Helpers\FakeLoader;
-use Grixu\Synchronizer\Tests\Helpers\FakeParser;
-use Grixu\Synchronizer\Tests\Helpers\FakeSyncConfig;
+use Grixu\Synchronizer\Tests\Helpers\FakeEngineConfig;
+use Grixu\Synchronizer\Tests\Helpers\FakeProcessConfig;
 use Grixu\Synchronizer\Tests\Helpers\SyncTestCase;
-use Illuminate\Database\Eloquent\Model;
+use Grixu\Synchronizer\Tests\Helpers\TestErrorHandler;
+use Grixu\Synchronizer\Tests\Helpers\TestSyncHandler;
 use Illuminate\Http\Client\Request;
-use Illuminate\Queue\SerializableClosure;
 use Illuminate\Support\Facades\Bus;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Http;
@@ -18,43 +17,34 @@ use Illuminate\Support\Facades\Queue;
 
 class StartSyncActionTest extends SyncTestCase
 {
-    protected StartSyncAction $obj;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->obj = new StartSyncAction();
-    }
-
     /** @test */
     public function full_sync()
     {
-        $this->runBatchAndCheckIt(
+        $config = [
             [
-                'customer' => FakeSyncConfig::makeArray(),
-                'another_but_same' => FakeSyncConfig::makeArray(),
-            ]
-        );
+                'process' => FakeProcessConfig::make(),
+                'engine' => FakeEngineConfig::make(),
+            ],
+        ];
+        $obj = new StartSyncAction($config);
+
+        $this->runBatchAndCheckIt($obj, $config);
     }
 
-    protected function runBatchAndCheckIt(array $config)
+    protected function runBatchAndCheckIt(StartSyncAction $obj, array $configs): \Illuminate\Bus\Batch
     {
         Queue::fake();
         $bus = Bus::fake();
 
-        $this->obj->execute($config);
+        $batch = $obj->dispatch();
 
         $bus->assertBatched(
-            function ($batch) use ($config) {
-                return $batch->jobs->count() == count($config);
+            function ($batch) use ($configs) {
+                return $batch->jobs->count() == count($configs);
             }
         );
-    }
 
-    /** @test */
-    public function one_module()
-    {
-        $this->runBatchAndCheckIt([FakeSyncConfig::makeArray()]);
+        return $batch;
     }
 
     /** @test */
@@ -63,22 +53,12 @@ class StartSyncActionTest extends SyncTestCase
         Http::fake();
 
         $config = [
-            'customer' => [
-                FakeLoader::class,
-                FakeParser::class,
-                Model::class,
-                'xlId',
-                null,
-                null,
-                [],
-                [],
-                new SerializableClosure(function () {
-                    Http::get('http://testable.dev');
-                }),
-            ],
+            'process' => FakeProcessConfig::make(error: TestErrorHandler::class),
+            'engine' => FakeEngineConfig::make(),
         ];
+        $obj = new StartSyncAction($config);
 
-        $this->obj->execute($config, 'sync');
+        $this->runBatchAndCheckIt($obj, $config);
 
         Http::assertSent(function (Request $request) {
             return $request->url() == 'http://testable.dev';
@@ -88,27 +68,15 @@ class StartSyncActionTest extends SyncTestCase
     /** @test */
     public function it_could_use_array_of_sync_configs_too()
     {
-        $this->runBatchAndCheckIt(
+        $config = [
             [
-                'customer' => FakeSyncConfig::make(),
-                'another_but_same' => FakeSyncConfig::make(),
-            ]
-        );
-    }
+                'process' => FakeProcessConfig::makeArray(),
+                'engine' => FakeEngineConfig::makeArray(),
+            ],
+        ];
+        $obj = new StartSyncAction($config);
 
-    /** @test */
-    public function it_could_use_single_config_object_too()
-    {
-        Queue::fake();
-        $bus = Bus::fake();
-
-        $this->obj->execute(FakeSyncConfig::make());
-
-        $bus->assertBatched(
-            function ($batch) {
-                return $batch->jobs->count() == 1;
-            }
-        );
+        $this->runBatchAndCheckIt($obj, $config);
     }
 
     /** @test */
@@ -116,11 +84,15 @@ class StartSyncActionTest extends SyncTestCase
     {
         Event::fake();
 
-        $batch = $this->obj->execute(
+        $config = [
             [
-                'customer' => FakeSyncConfig::make(),
-            ]
-        );
+                'process' => FakeProcessConfig::make(),
+                'engine' => FakeEngineConfig::make(),
+            ],
+        ];
+        $obj = new StartSyncAction($config);
+
+        $batch = $this->runBatchAndCheckIt($obj, $config);
 
         $this->assertTrue($batch->finished());
         Event::assertDispatched(CollectionSynchronizedEvent::class, 1);
@@ -131,12 +103,13 @@ class StartSyncActionTest extends SyncTestCase
     {
         Http::fake();
 
-        $config = FakeSyncConfig::make();
-        $config->setSyncClosure(new SerializableClosure(function () {
-            Http::get('http://testable.dev');
-        }));
+        $config = [
+            'process' => FakeProcessConfig::make(sync: TestSyncHandler::class),
+            'engine' => FakeEngineConfig::make(),
+        ];
+        $obj = new StartSyncAction($config);
 
-        $this->obj->execute($config);
+        $this->runBatchAndCheckIt($obj, $config);
 
         Http::assertSent(function (Request $request) {
             return $request->url() == 'http://testable.dev';
