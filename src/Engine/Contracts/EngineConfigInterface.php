d8a1399c8d354cdd6220cd2b3cfcac8570218344
Mateusz Gostański <mateusz.gostanski@gmail.com>
2021-08-27 14:08:59 +0200

    Updated EngineConfigInterface

--- src/Engine/Contracts/EngineConfigInterface.php
+++ src/Engine/Contracts/EngineConfigInterface.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace Grixu\Synchronizer\Config\Contracts;
+namespace Grixu\Synchronizer\Engine\Contracts;
 
 interface EngineConfigInterface
 {
     public function getModel(): string;
     public function getKey(): string;
     public function getIds(): array;
+    public function getChecksumFieldAsDtoField(): string | null;
+    public function getChecksumFieldAsModelField(): string | null;
     public function getTimestamps(): array;
-    public function getTimestampsAsSnake(): array;
-    public function getChecksumField(): string | null;
-    public function getChecksumFieldAsSnake(): string | null;
     public function getExcluded(): array;
     public function getFillable(): array;
     public function getOnly(): array;
